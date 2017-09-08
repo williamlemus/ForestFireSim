86f3deb8a33760e6b912ab95d4a4e2e2982b1c6a
William Lemus <william.lemus@gmail.com>
2017-09-08 17:29:35 -0400

    Add user option to pick random point. Start work on resetting map

--- entry.js
+++ entry.js
@@ -3,4 +3,9 @@ import Grid from './lib/grid';
 document.addEventListener('DOMContentLoaded', () => {
   const grid = new Grid();
   grid.render();
+  const randomButton = document.getElementById('random-button');
+  randomButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    grid.handleRandomClick();
+  });
 });
