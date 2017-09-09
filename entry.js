63964b19db0548c0c9e6cfe88825d9bca8666815
William Lemus <william.lemus@gmail.com>
2017-09-08 21:10:15 -0400

    User can now reset map

--- entry.js
+++ entry.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = new Grid();
   grid.render();
   const randomButton = document.getElementById('random-button');
+  const resetButton = document.getElementById('reset-button');
   randomButton.addEventListener('click', (e) => {
     e.preventDefault();
     grid.handleRandomClick();
+  });
+  resetButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    grid.handleResetClick();
   });
 });
